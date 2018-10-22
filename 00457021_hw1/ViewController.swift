1a2f4e642161a7e665eb92247e46895a5358b3e9
User20 <user20@imac-20.local>
2018-10-22 16:40:35 +0800

    Initial Commit

---
+++ 00457021_hw1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  00457021_hw1
+//
+//  Created by User20 on 2018/10/22.
+//  Copyright © 2018 User20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
